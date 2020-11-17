b5d3683b3aa2cff2e4b498ff5ddc4a929b1c8844
arsnatenkov <arsnatenkov@edu.hse.ru>
2020-11-17 15:12:24 +0300

    new fixes

--- HOMEWORK3/Bees/main.cpp
+++ HOMEWORK3/Bees/main.cpp
@@ -2,8 +2,9 @@
 #include <pthread.h>
 #include <vector>
 #include <random>
+#include <ctime>
 
-int forest[10][10]; //Лес в котором летают неправильные пчелы
+std::vector<std::vector<int>> forest; //Лес в котором летают неправильные пчелы
 volatile bool found = false; //Флаг для проверки нашелся ли Пух
 int str; //Переменная для хранения номера задачи
 int ansM, ansN; //Переменные для хранения ответа
@@ -15,7 +16,7 @@ void* searching(void *count) {
 
     bool complete = false; //Проверка готовности
     if(!found){ //Если Пух не наден, то давайте искать его на этой линии в лесу
-        for (int i = 0; i < 10; ++i) {
+        for (int i = 0; i < forest[0].size(); ++i) {
             if (forest[(int64_t)count][i] == 1) {
 
                 found = true;
@@ -23,6 +24,8 @@ void* searching(void *count) {
                 std::cout << str + 1 << " detachment of wrong bees found Pooh\n";
                 ansN = str;
                 ansM = i;
+                std::cout << "Pooh located:" << ansN + 1 << " " << ansM + 1 << '\n';
+                exit(0);
             }
         }
     }
@@ -33,35 +36,40 @@ void* searching(void *count) {
 
 }
 /// Вход в программу, определение положения Пух в лесу
-int main()
-{
-    int n = 10;
-    for (int i = 0; i < 10; ++i){
-        for (int j = 0; j < 10; ++j) {
-            forest[i][j] = 0;
+int main() {
+    int a;
+    int b;
+    std::cout << "Enter x: ";
+    std::cin >> a;
+    std::cout << "Enter y: ";
+    std::cin >> b;
+
+
+    for (int i = 0; i < a; ++i) {
+        forest.push_back(std::vector<int> ());
+        for (int j = 0; j < b; ++j) {
+            forest[i].push_back(0);
         }
     }
-    int x = rand() % 11; //Координата Пуха в лесу
-    int y = rand() % 11; //Координата Пуха в лесу
+    srand(time(NULL));
+    int x = rand() % a; //Координата Пуха в лесу
+    int y = rand() % b; //Координата Пуха в лесу
     forest[x][y] = 1;
 
+    int n = a;
     std::vector<pthread_t> threads(n); //Создание портфеля задач
 
-    for (std::vector<pthread_t>::iterator it = threads.begin(); it != threads.end(); ++it){ //Выполнение задач в портфеле
+    for (std::vector<pthread_t>::iterator it = threads.begin();
+         it != threads.end(); ++it) { //Выполнение задач в портфеле
 
-        pthread_create(&*it, NULL, searching, (void *)str);
-        pthread_join((pthread_t)*it, NULL);
+        pthread_create(&*it, NULL, searching, (void *) str);
+        pthread_join((pthread_t) *it, NULL);
         ++str;
     }
     //Вывод местоположения Пуха в лесу
-    if(x < 0 || y < 0 || x > 10 || y > 10){
+    if (x < 0 || y < 0 || x > a || y > b) {
         std::cout << "Pooh wasn't in the forest";
-    }
-    else{
-        std::cout << "Pooh located:" << ansN + 1 << " " << ansM + 1 << '\n';
     }
 
     return 0;
 }
-
-
